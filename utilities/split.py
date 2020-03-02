ff81b19c0cdbb23a4ac5cefe482223840c670b4d
Gary Ong <garyong.ahs@gmail.com>
2020-03-02 20:43:44 +0800

    shuffled the data

--- utilities/split.py
+++ utilities/split.py
@@ -7,6 +7,7 @@ x = np.load("train.npy")
 
 def make_iter_splits(nfold):
     train_df = pd.read_csv('bengaliai-cv19/train.csv')
+    train_df = train_df.sample(frac=1)
     train_df['id'] = train_df['image_id'].apply(lambda x: int(x.split('_')[1]))
 
     X, y = train_df[['id', 'grapheme_root', 'vowel_diacritic', 'consonant_diacritic']]\
@@ -39,18 +40,18 @@ test_index = train_df[train_df['fold']==3].index.values
 val_index = train_df[train_df['fold']==7].index.values
 train_index = train_df[train_df['fold'].isin([0,1,2,4,5,6,8,9])].index.values
 
-make_data("tvt_train_label.csv","tvt_train",train_index,train_df)
-make_data("tvt_val_label.csv","tvt_val",val_index,train_df)
-make_data("tvt_test_label.csv","tvt_test",test_index,train_df)
+make_data("shuffle_train_label.csv","shuffle_train",train_index,train_df)
+make_data("shuffle_val_label.csv","shuffle_val",val_index,train_df)
+make_data("shuffle_test_label.csv","shuffle_test",test_index,train_df)
 
 train_df = make_iter_splits(7)
-test_index = train_df[train_df['fold']==4].index.values
-val_index = train_df[train_df['fold']==9].index.values
-train_index = train_df[train_df['fold'].isin([0,1,2,3,5,6,7,8])].index.values
+test_index = train_df[train_df['fold']==2].index.values
+val_index = train_df[train_df['fold']==4].index.values
+train_index = train_df[train_df['fold'].isin([0,1,3,5,6])].index.values
 
-make_data("tvt_train_label2.csv","tvt_train2",train_index,train_df)
-make_data("tvt_val_label2.csv","tvt_val2",val_index,train_df)
-make_data("tvt_test_label2.csv","tvt_test2",test_index,train_df)
+make_data("shuffle_train_label2.csv","shuffle_train2",train_index,train_df)
+make_data("shuffle_val_label2.csv","shuffle_val2",val_index,train_df)
+make_data("shuffle_test_label2.csv","shuffle_test2",test_index,train_df)
 
 
 # for i in range(5):
